9457e2022e64eb15a95c37408c6c9327a6eaac1f
Markus <faulaner@googlemail.com>
2017-11-15 14:32:38 +0100

    added NetworkStates

--- Assets/Scripts/ServerClient.cs
+++ Assets/Scripts/ServerClient.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.SceneManagement;
 
 public class ServerClient : MonoBehaviour {
 
@@ -11,7 +13,11 @@ public class ServerClient : MonoBehaviour {
 	public Button serverButton;
 	public Button clientButton;
 	public Button sendButton;
+	public Button backButton;
+	public InputField serverIpInput;
 	public Text textField;
+	
+	public GameObject networkUI;
 	public const short MSG_ID = 213;
 	Vector3 scorePos = new Vector3(12,0,0);
     
@@ -28,6 +34,8 @@ public class ServerClient : MonoBehaviour {
 		sendButton = sendButton.GetComponent<Button>();
 		sendButton.onClick.AddListener(SendMessage);
 		
+		backButton.gameObject.SetActive(false);
+		backButton.onClick.AddListener(BackButtonClicked);
 	}
 	
 
@@ -40,7 +48,11 @@ public class ServerClient : MonoBehaviour {
 	public void SetupServer()
 	{
 		NetworkServer.Listen(8080);
-		textField.text = "Server listen";	
+		String serverIp = Network.player.ipAddress;
+		textField.text = "Server listen on IP: " + serverIp;
+		networkUI.SetActive(false);
+		backButton.gameObject.SetActive(true);
+		
 	}
 
 	public void SendMessage()
@@ -54,17 +66,47 @@ public class ServerClient : MonoBehaviour {
 	// Create a client and connect to the server port
 	public void SetupClient()
 	{
-		myClient = new NetworkClient();
-		myClient.RegisterHandler(MsgType.Connect, OnConnected);     
-		myClient.Connect("192.168.178.44", 8080);
-		myClient.RegisterHandler(MSG_ID,ReceiveMessage);
+		String serverIp = serverIpInput.text;
+		if (serverIp.Length == 0)
+		{
+			textField.text = "ServerIP eingeben!";
+		}
+		else
+		{
+			myClient = new NetworkClient();
+			myClient.RegisterHandler(MsgType.Connect, OnConnected);     
+			myClient.RegisterHandler(MsgType.Disconnect, OnDisconnected);     
+			myClient.Connect(serverIp, 8080);
+			myClient.RegisterHandler(MSG_ID, ReceiveMessage);
+		}
 	}
     
 	// client function
-	public void OnConnected(NetworkMessage netMsg)
+	public void OnConnected(NetworkMessage message)
 	{
 		Debug.Log("Connected to server");
 		textField.text = "Connected to server";
+		ChangeNetworkUiVisibility(false);
+		Camera.main.clearFlags = CameraClearFlags.Skybox;
+	}
+
+	public void OnDisconnected(NetworkMessage message)
+	{
+		Debug.Log("Disconnected from server");
+		textField.text = "Disconnected from server";
+		ChangeNetworkUiVisibility(true);
+		Camera.main.clearFlags = CameraClearFlags.SolidColor;
+		myClient = null;
+	}
+
+	private void Disconnect()
+	{
+		Debug.Log("Disconnect client");
+		textField.text = "Disconnected from server";
+		ChangeNetworkUiVisibility(true);
+		Camera.main.clearFlags = CameraClearFlags.SolidColor;
+		myClient.Disconnect();
+		myClient = null;
 	}
 	
 	private void ReceiveMessage(NetworkMessage message)
@@ -73,4 +115,25 @@ public class ServerClient : MonoBehaviour {
 		string text = message.ReadMessage<StringMessage> ().value;
 		textField.text = text;
 	}
+
+	private void BackButtonClicked()
+	{
+		if (myClient != null)
+		{
+				Disconnect();
+		}
+		else if (NetworkServer.active)
+		{
+				NetworkServer.Shutdown();
+				ChangeNetworkUiVisibility(true);
+				textField.text = "Server closed";
+		}
+	}
+
+	private void ChangeNetworkUiVisibility(bool visibility)
+	{
+		networkUI.SetActive(visibility);
+		backButton.gameObject.SetActive(!visibility);
+	}
+	
 }

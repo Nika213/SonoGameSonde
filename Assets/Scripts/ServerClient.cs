75d95104d9a1eafd3db74316b7cd34c6ae0b9733
Markus <faulaner@googlemail.com>
2017-11-19 18:23:26 +0100

    added Tango

--- Assets/Scripts/ServerClient.cs
+++ Assets/Scripts/ServerClient.cs
@@ -9,16 +9,28 @@ using UnityEngine.SceneManagement;
 
 public class ServerClient : MonoBehaviour {
 
-	public bool isAtStartup = true;
+	public bool isSonde = false;
 	public Button serverButton;
 	public Button clientButton;
 	public Button sendButton;
 	public Button backButton;
+	public Button SondeButton;
+	public GameObject DevicePosition;
+	public GameObject Client3DUI;
+	public GameObject Cube;
+	
 	public InputField serverIpInput;
 	public Text textField;
+	public Text PositinoDebugText;
+	public Text FactorText;
+	public Slider MovementFactorSlider;
 	
 	public GameObject networkUI;
 	public const short MSG_ID = 213;
+
+	public float movemendSpeedFactor = 100f;
+	
+	private Transform offset;
 	Vector3 scorePos = new Vector3(12,0,0);
     
 	NetworkClient myClient;
@@ -36,8 +48,48 @@ public class ServerClient : MonoBehaviour {
 		
 		backButton.gameObject.SetActive(false);
 		backButton.onClick.AddListener(BackButtonClicked);
+		
+		SondeButton.gameObject.SetActive(false);
+		SondeButton.onClick.AddListener(SondeButtonClicked);
+		
+		Client3DUI.gameObject.SetActive(false);
+		MovementFactorSlider.gameObject.SetActive(false);
+		FactorText.gameObject.SetActive(false);
+		PositinoDebugText.gameObject.SetActive(false);
+		offset = DevicePosition.transform;
 	}
-	
+
+	private void SondeButtonClicked()
+	{
+		isSonde = !isSonde;
+		SondeButton.gameObject.GetComponent<Text>().text = isSonde ? "Stop Sonde" : "Start Sonde";
+	}
+
+	private void Update()
+	{
+		if (NetworkServer.active && isSonde)
+		{
+//			movemendSpeedFactor = MovementFactorSlider.value;
+//			FactorText.text = movemendSpeedFactor.ToString();
+			PositinoDebugText.text = "x: " + DevicePosition.transform.localPosition.x + "\n y: " + DevicePosition.transform.localPosition.y + "\n z: " + DevicePosition.transform.localPosition.z + "\n Quaternium: " + DevicePosition.transform.localRotation;
+			Vector3 localPosition = DevicePosition.transform.localPosition;
+			
+			TransfromObject transfromObject = new TransfromObject();
+			transfromObject.x = (localPosition.x - offset.localPosition.x) * movemendSpeedFactor;
+			transfromObject.y = (localPosition.y - offset.localPosition.y) * movemendSpeedFactor;
+			transfromObject.z = (localPosition.z - offset.localPosition.z) * movemendSpeedFactor;
+			
+			Quaternion localRoation = DevicePosition.transform.localRotation;
+			transfromObject.qx = localRoation.x;
+			transfromObject.qy = localRoation.y;
+			transfromObject.qz = localRoation.z;
+			transfromObject.qw = localRoation.w;
+			
+			StringMessage positionMessage = new StringMessage();
+			positionMessage.value = JsonUtility.ToJson(transfromObject);
+			NetworkServer.SendToAll(MSG_ID, positionMessage);
+		}
+	}
 
 	public struct ScoreMessage
 	{
@@ -52,7 +104,11 @@ public class ServerClient : MonoBehaviour {
 		textField.text = "Server listen on IP: " + serverIp;
 		networkUI.SetActive(false);
 		backButton.gameObject.SetActive(true);
-		
+		SondeButton.gameObject.SetActive(true);
+		MovementFactorSlider.gameObject.SetActive(true);
+		FactorText.gameObject.SetActive(true);
+		PositinoDebugText.gameObject.SetActive(true);
+		Screen.orientation = ScreenOrientation.Portrait;
 	}
 
 	public void SendMessage()
@@ -87,6 +143,7 @@ public class ServerClient : MonoBehaviour {
 		Debug.Log("Connected to server");
 		textField.text = "Connected to server";
 		ChangeNetworkUiVisibility(false);
+		Client3DUI.gameObject.SetActive(true);
 		Camera.main.clearFlags = CameraClearFlags.Skybox;
 	}
 
@@ -105,6 +162,11 @@ public class ServerClient : MonoBehaviour {
 		textField.text = "Disconnected from server";
 		ChangeNetworkUiVisibility(true);
 		Camera.main.clearFlags = CameraClearFlags.SolidColor;
+		SondeButton.gameObject.SetActive(false);
+		Client3DUI.gameObject.SetActive(false);
+		MovementFactorSlider.gameObject.SetActive(false);
+		FactorText.gameObject.SetActive(false);
+		PositinoDebugText.gameObject.SetActive(false);
 		myClient.Disconnect();
 		myClient = null;
 	}
@@ -112,8 +174,12 @@ public class ServerClient : MonoBehaviour {
 	private void ReceiveMessage(NetworkMessage message)
 	{
 		//reading message
-		string text = message.ReadMessage<StringMessage> ().value;
-		textField.text = text;
+		string messageValue = message.ReadMessage<StringMessage> ().value;
+
+		TransfromObject transfromObject = JsonUtility.FromJson<TransfromObject>(messageValue);
+		Cube.transform.localPosition = new Vector3(transfromObject.x, transfromObject.y, transfromObject.z);
+		Cube.transform.localRotation = new Quaternion(transfromObject.qx, transfromObject.qy, transfromObject.qz, transfromObject.qw);
+		
 	}
 
 	private void BackButtonClicked()
@@ -124,9 +190,15 @@ public class ServerClient : MonoBehaviour {
 		}
 		else if (NetworkServer.active)
 		{
-				NetworkServer.Shutdown();
-				ChangeNetworkUiVisibility(true);
-				textField.text = "Server closed";
+			NetworkServer.Shutdown();
+			ChangeNetworkUiVisibility(true);
+			SondeButton.gameObject.SetActive(false);
+			Client3DUI.gameObject.SetActive(false);
+			MovementFactorSlider.gameObject.SetActive(false);
+			FactorText.gameObject.SetActive(false);
+			PositinoDebugText.gameObject.SetActive(false);
+			textField.text = "Server closed";
+			Screen.orientation = ScreenOrientation.Landscape;
 		}
 	}
 
@@ -134,6 +206,20 @@ public class ServerClient : MonoBehaviour {
 	{
 		networkUI.SetActive(visibility);
 		backButton.gameObject.SetActive(!visibility);
+	}
+	
+	[Serializable]
+	public class TransfromObject
+	{
+		public float x;
+		public float y;
+		public float z;
+		
+		public float qx;
+		public float qy;
+		public float qz;
+		public float qw;
+
 	}
 	
 }
